b8c392e0b954acbde4acbe2eabab1644b6260ac8
Joshua Simmons <josh@phinsecurity.com>
2023-09-03 00:13:39 -0400

    This should work

--- test-fast-doc/lib/fast-docs-lib.mjs
+++ test-fast-doc/lib/fast-docs-lib.mjs
@@ -21,6 +21,7 @@ function matchWithLineNumber(str, re) {
 async function getFastDocItems() {
     const markdowns = await findDownAll('.md')
 
+    //this is a bit of a hack, but it works
     const fastDocLinks = []
     const fastDocCodeBlocks = []
     for (const file of markdowns) {

1c07b77930335616f2fc756ac395f627d01a44f8
Tedis Agolli <tedis@hey.com>
2023-09-06 16:17:35 -0400

    Add prettier and format js

--- syncBlocks.mjs
+++ syncBlocks.mjs
@@ -1,55 +1,80 @@
-import fs from "fs"
-import path from "path"
-import { getItems } from './lib/freshdoc-lib.mjs'
+import fs from "fs";
+import path from "path";
+import { getItems } from "./lib/freshdoc-lib.mjs";
 
 export async function syncAllBlocks() {
-    const { codeBlocks } = await getItems()
+  const { codeBlocks } = await getItems();
 
-    //consolidate codeBlocks for the same source file
-    const codeBlocksBySource = {}
-    for (const codeBlock of codeBlocks) {
-        if (!codeBlocksBySource[codeBlock.sourceMarkdown]) {
-            codeBlocksBySource[codeBlock.sourceMarkdown] = []
-        }
-        codeBlocksBySource[codeBlock.sourceMarkdown].push(codeBlock)
+  //consolidate codeBlocks for the same source file
+  const codeBlocksBySource = {};
+  for (const codeBlock of codeBlocks) {
+    if (!codeBlocksBySource[codeBlock.sourceMarkdown]) {
+      codeBlocksBySource[codeBlock.sourceMarkdown] = [];
     }
+    codeBlocksBySource[codeBlock.sourceMarkdown].push(codeBlock);
+  }
 
-    for (const codeBlockList of Object.values(codeBlocksBySource).sort((a, b) => a[0].fastDocLineNumber - b[0].fastDocLineNumber)) {
-        const markdownFileContents = fs.readFileSync(codeBlockList[0].sourceMarkdown, 'utf8')
-        let markdownLines = markdownFileContents.split("\n")
-        for (const [i, codeBlock] of codeBlockList.entries()) {
-            const { sourceMarkdown, referencedCodeFilename,
-                codeBlockRangeStart, codeBlockRangeEnd, markdownFreshDocReferenceLineNumber,
-            } = codeBlock
+  for (const codeBlockList of Object.values(codeBlocksBySource).sort(
+    (a, b) => a[0].fastDocLineNumber - b[0].fastDocLineNumber,
+  )) {
+    const markdownFileContents = fs.readFileSync(
+      codeBlockList[0].sourceMarkdown,
+      "utf8",
+    );
+    let markdownLines = markdownFileContents.split("\n");
+    for (const [i, codeBlock] of codeBlockList.entries()) {
+      const {
+        sourceMarkdown,
+        referencedCodeFilename,
+        codeBlockRangeStart,
+        codeBlockRangeEnd,
+        markdownFreshDocReferenceLineNumber,
+      } = codeBlock;
 
-            const fileContents = fs.readFileSync(referencedCodeFilename, 'utf8')
-            const lines = fileContents.split("\n")
-            const codeBlockLines = lines.slice(codeBlockRangeStart - 1, codeBlockRangeEnd)
-            const previousLines = markdownLines.slice(0, markdownFreshDocReferenceLineNumber)
-            //the next lines should be the lines after the codeBlock
-            const endOfCodeBlockRegex = /```/gi
-            let endOfMarkdownCodeBlock = markdownFreshDocReferenceLineNumber
-            let numberOfDeletedLines = 0
-            for (let i = markdownFreshDocReferenceLineNumber; i < markdownLines.length; i++) {
-                if (endOfCodeBlockRegex.test(markdownLines[i])) {
-                    endOfMarkdownCodeBlock = i
-                    break
-                } else {
-                    numberOfDeletedLines++
-                }
-            }
-
-            const nextLines = markdownLines.slice(endOfMarkdownCodeBlock)
-
-            markdownLines = [...previousLines,
-                 `//${path.relative(process.env.PWD, referencedCodeFilename)}:${codeBlockRangeStart}-${codeBlockRangeEnd}`,
-                 ...codeBlockLines,
-                  ...nextLines]
-            //update future codeBlocks in the list to properly offset by added lines
-            for (const futureCodeBlock of codeBlockList.slice(i)) {
-                futureCodeBlock.markdownFreshDocReferenceLineNumber += codeBlockLines.length - numberOfDeletedLines + 1 // for the reference!
-            }
-            fs.writeFileSync(sourceMarkdown, markdownLines.join("\n"))
+      const fileContents = fs.readFileSync(referencedCodeFilename, "utf8");
+      const lines = fileContents.split("\n");
+      const codeBlockLines = lines.slice(
+        codeBlockRangeStart - 1,
+        codeBlockRangeEnd,
+      );
+      const previousLines = markdownLines.slice(
+        0,
+        markdownFreshDocReferenceLineNumber,
+      );
+      //the next lines should be the lines after the codeBlock
+      const endOfCodeBlockRegex = /```/gi;
+      let endOfMarkdownCodeBlock = markdownFreshDocReferenceLineNumber;
+      let numberOfDeletedLines = 0;
+      for (
+        let i = markdownFreshDocReferenceLineNumber;
+        i < markdownLines.length;
+        i++
+      ) {
+        if (endOfCodeBlockRegex.test(markdownLines[i])) {
+          endOfMarkdownCodeBlock = i;
+          break;
+        } else {
+          numberOfDeletedLines++;
         }
+      }
+
+      const nextLines = markdownLines.slice(endOfMarkdownCodeBlock);
+
+      markdownLines = [
+        ...previousLines,
+        `//${path.relative(
+          process.env.PWD,
+          referencedCodeFilename,
+        )}:${codeBlockRangeStart}-${codeBlockRangeEnd}`,
+        ...codeBlockLines,
+        ...nextLines,
+      ];
+      //update future codeBlocks in the list to properly offset by added lines
+      for (const futureCodeBlock of codeBlockList.slice(i)) {
+        futureCodeBlock.markdownFreshDocReferenceLineNumber +=
+          codeBlockLines.length - numberOfDeletedLines + 1; // for the reference!
+      }
+      fs.writeFileSync(sourceMarkdown, markdownLines.join("\n"));
     }
+  }
 }
